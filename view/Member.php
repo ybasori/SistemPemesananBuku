78a060163ad5b2b6fcf7194f951590bfd807de77
gitacitrap <puspita.gitacitra@gmail.com>
2017-02-01 23:31:34 +0700

    Memperbaiki modal update dan memperbaiki delete

--- view/Member.php
+++ view/Member.php
@@ -1,11 +1,5 @@
 <main class="col-xs-12 col-sm-9 col-lg-10 pull-right" style="top:55px;">
 	<div class="col-sm-12 panel panel-default" style="padding-top: 15px">
-		<div class="panel-header">				
-		<!-- panel header -->
-			<button class="btn btn-primary " data-toggle="modal" data-target="#modal-produk-add"><i class="glyphicon glyphicon-plus-sign"></i> Tambah</button>
-		</div>
-		<!-- END Panel Header -->					
-		<!-- view data -->
 	    <div class="panel-body">
 			<table id="tabel_data_member" class="table table-bordered table-striped">
 				<thead>
@@ -15,27 +9,73 @@
 						<th>Nama Lengkap</th>
 						<th>Email</th>
 						<th>Level</th>
-						<th style="text-align: center;">Action</th>
+						<th>Action</th>
 					</tr>
 				</thead>						
 			</table>                   
         </div><!-- END-Panel-body -->
 	</div><!--END PANEL-->
 </main><!--END MAIN -->
+
+<!-- Modal Edit Produk-->
+	<div id="modal-member-edit" class="modal fade" role="dialog">
+  		<div class="modal-dialog">    
+    		<div class="modal-content">
+      			<div class="modal-header">
+        			<button type="button" class="close" data-dismiss="modal">&times;</button>
+        			<h4 class="modal-title">Update Member</h4>
+      			</div>
+      			<!-- form edit -->
+        		<form action="<?php echo $sys->base_url() ?>/action/produk.php" method="post" enctype="multipart/form-data" accept-charset="utf-8" role="form" class="form-horizontal">
+      				<div class="modal-body">
+        				<div class="form-group">
+        					<label class="control-label col-sm-3" for="nama">Nama</label>
+        					<div class="col-sm-7">
+        						<input type="text" name="nama" id="edit_nama" class="form-control" disabled>
+        					</div>
+        				</div>
+        				<div class="form-group">
+        					<label class="control-label col-sm-3" for="email">Email</label>
+        					<div class="col-sm-7">
+        						<input type="text" name="email" id="edit_email" class="form-control" disabled>
+        					</div>
+        				</div>
+        				<div class="form-group">
+        					<label class="control-label col-sm-3" for="level">Level</label>
+        					<div class="col-sm-7">
+        						<select name="level" id="edit_level" class="form-control">
+        							<option value="member">member</option>
+        							<option value="master">master</option>
+        						</select> 
+        					</div>
+        				</div>
+	      			</div>
+	      			<div class="modal-footer">
+	      				<button type="submit" class="btn btn-primary" name="btn_edit_produk">Simpan</button>
+	        			<button type="button" class="btn btn-default" data-dismiss="modal">Keluar</button>
+	      			</div>
+      			</form>
+        			<!--END FORM EDIT-->
+    		</div>
+
+  		</div>
+	</div>
+<!--END MODAL-->
+
 <script type="text/javascript">
 	var t = $('#tabel_data_member').DataTable({
 		  "autoWidth": false,
 		  "rowCallback": function( row, data, index ) {
 		  	$('td:eq(1)', row).html("<img style=\"width:100px\" src=\"<?php echo $sys->base_url(); ?>/"+data[1]+"\">");
-			 $('td:eq(5)', row).html("<button class=\"btn btn-warning update-form\" data-toggle=\"modal\" data-target=\"#mymodalupdate\" data-id=\""+data[0]+"\"><i class=\"glyphicon glyphicon-pencil\"></i> Ubah</button>&nbsp;&nbsp;<button class=\"btn btn-danger delete-form\" data-toggle=\"modal\" data-target=\"#mymodaldelete\" data-id=\""+data[0]+"\" ><i class=\"glyphicon glyphicon-trash\"></i> Hapus</button>");
+			 $('td:eq(5)', row).html("<button class=\"btn btn-warning update-form\" data-toggle=\"modal\" data-target=\"#modal-member-edit\" data-id=\""+data[0]+"\"><i class=\"glyphicon glyphicon-pencil\"></i> Ubah</button>&nbsp;&nbsp;<button class=\"btn btn-danger delete-form\" data-id=\""+data[0]+"\" ><i class=\"glyphicon glyphicon-trash\"></i> Hapus</button>");
 		  },			  
 		  "columnDefs": [
-				{ "width": "2%", "targets": 0 },
-				{ "width": "15%", "targets": 1 },
-				{ "width": "20%", "targets": 2 },
-				{ "width": "20%", "targets": 3 },
-				{ "width": "20%", "targets": 4 },
-				{ "width": "20%", "targets": 5 }
+				{ "width": "2%",sClass: "dt-head-center dt-body-center", "targets": 0 },
+				{ "width": "10%",sClass: "dt-head-center dt-body-center", "targets": 1 },
+				{ "width": "20%",sClass: "dt-head-center dt-body-left", "targets": 2 },
+				{ "width": "10%",sClass: "dt-head-center dt-body-center", "targets": 3 },
+				{ "width": "10%",sClass: "dt-head-center dt-body-center", "targets": 4 },
+				{ "width": "20%",sClass: "dt-head-center dt-body-center", "targets": 5 }
 				]
 	});	
 
@@ -60,7 +100,7 @@
   				$data [$i][1] = "foto_member/default_foto_member.png";
   				$data [$i][2] = "Nama Lengkap";
   				$data [$i][3] = "Email";
-  				$data [$i][4] = "Level";
+  				$data [$i][4] = "member";
   				$data [$i][5] =$i;
   			}
 			echo "
@@ -69,6 +109,60 @@
 			add_data_to_table_t(t,data);
 			";
   		?> 
-	  	t.order( [ 0, 'asc' ] ).draw(false);	  		
+	  	t.order( [ 0, 'asc' ] ).draw(false);	
+	  	$(".update-form").click(function(){
+	  		var indeks = $(this).data('id');
+	  		$("#edit_nama").val(data[indeks][2]); 
+	  		$("#edit_email").val(data[indeks][3]); 
+	  		$("#edit_level").val(data[indeks][4]); 
+	  	});
+
+	  	//sweetalert
+	  	$(".delete-form").click(function(){
+	  		var indeks = $(this).data('id');
+	  		console.log("id="+data[indeks][5]);
+		  	swal({
+				title: "Apakah Anda Yakin?",
+				text: "Jika Anda Menghapus Data Member Ini, Maka Data Tersebut Tidak Dapat Dikembalikan!",
+				type: "warning",
+				showCancelButton: true,
+				confirmButtonColor: "#DD6B55",
+				confirmButtonText: "Ya! Hapus",
+				cancelButtonText: "Batal",
+				closeOnConfirm: false,
+				closeOnCancel: false,
+				showLoaderOnConfirm: true
+			},
+			function(isConfirm){
+  				if (isConfirm) {
+  					 $.ajax({
+		                url: "<?php echo $sys->base_url() ?>/action/member.php",
+		                type: "POST",
+		                data: "id="+data[indeks][5],
+		                dataType: "html",
+		                success: function(data){
+		                        if (data==1){
+		                            setTimeout(function(){
+		                                swal({
+		                                    title: "Sukses",
+		                                    text: "Data Telah Dihapus!",
+		                                    type: "success"
+		                                }, function(){
+		                                    window.location.reload(true);
+		                                });
+		                            }, 2000);
+		                        }
+		                },
+		                error: function (xhr, ajaxOptions, thrownError) {
+		                    setTimeout(function(){
+		                        swal("Error!", "Silahkan Perikas Koneksi dan Ulangi", "error");
+		                            }, 2000);}
+		                 
+		            });
+  				} else {
+	    			swal("Batal", "Anda Tidak Jadi Menghapus Data Member :)", "error");
+  				}
+			});
+	  	});	  	  		
 	});	
 </script>
